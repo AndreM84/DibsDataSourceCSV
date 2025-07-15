60d5414e0c5bc352cecd744fd6827d38ef80f27b
wailsamjouni <waill.samjouni@gmail.com>
2025-07-15 15:43:35 +0200

    correct the encoding in utils_readcsv

--- src/dibs_datasource_csv/utils/utils_readcsv.py
+++ src/dibs_datasource_csv/utils/utils_readcsv.py
@@ -21,7 +21,7 @@ def read_user_building(data_path: str) -> pd.DataFrame | None:
     # path_parts = data_path.split("/")
     # file_path = os.path.join(*path_parts)
 
-    return pd.read_csv(data_path, sep=";", index_col=False, encoding="utf8")
+    return pd.read_csv(data_path, sep=";", index_col=False, encoding="utf-8")
 
 
 def read_user_buildings(data_path: str) -> pd.DataFrame | None:
